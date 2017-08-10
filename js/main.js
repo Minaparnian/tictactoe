f4ddf90695df673c32a5054c26a1cfcbc223bc4b
Mina Parnian <minaparnian66@gmail.com>
2017-08-10 17:32:19 +1000

    tictactoe project

--- js/main.js
+++ js/main.js
@@ -2,33 +2,35 @@
 
 let player1 = "X";
 let scoreX = "X";
-let array = ["box0", "box1", "box2", "box3", "box4", "box5", "box6", "box7", "box8"];
+let score1 = 0;
+let score2 = 0;
+let boardArray = ["box0", "box1", "box2", "box3", "box4", "box5", "box6", "box7", "box8"];
 
 let matchBoxes = function() {
 
-  if (array[0] === array[1] && array[1] === array[2]) {
+  if (boardArray[0] === boardArray[1] && boardArray[1] === boardArray[2]) {
     // $('#0,#1,#2').addClass('zoomOut');
     winAnimation(0, 1, 2)
     return true;
-  } else if (array[3] === array[4] && array[4] === array[5]) {
+  } else if (boardArray[3] === boardArray[4] && boardArray[4] === boardArray[5]) {
     winAnimation(3, 4, 5)
     return true;
-  } else if (array[6] === array[7] && array[7] === array[8]) {
+  } else if (boardArray[6] === boardArray[7] && boardArray[7] === boardArray[8]) {
     winAnimation(6, 7, 8)
     return true;
-  } else if (array[0] === array[3] && array[3] === array[6]) {
+  } else if (boardArray[0] === boardArray[3] && boardArray[3] === boardArray[6]) {
     winAnimation(0, 3, 6)
     return true;
-  } else if (array[1] === array[4] && array[4] === array[7]) {
+  } else if (boardArray[1] === boardArray[4] && boardArray[4] === boardArray[7]) {
     winAnimation(1, 4, 7)
     return true;
-  } else if (array[2] === array[5] && array[5] === array[8]) {
+  } else if (boardArray[2] === boardArray[5] && boardArray[5] === boardArray[8]) {
     winAnimation(2, 5, 8)
     return true;
-  } else if (array[0] === array[4] && array[4] === array[8]) {
+  } else if (boardArray[0] === boardArray[4] && boardArray[4] === boardArray[8]) {
     winAnimation(0, 4, 8)
     return true;
-  } else if (array[2] === array[4] && array[4] === array[6]) {
+  } else if (boardArray[2] === boardArray[4] && boardArray[4] === boardArray[6]) {
     winAnimation(2, 4, 6)
     return true;
   } else {
@@ -39,36 +41,70 @@ let matchBoxes = function() {
 };
 
 const winAnimation = function(a, b, c) {
-  $(`#${a}, #${b}, #${c}`).addClass('zoomOut').addClass('red');
+  $(`#${a}, #${b}, #${c}`)
 
 } // I have two one way like this and another is add this
 // $('#0,#1,#2').addClass('zoomOut');
 //bfore return for all of my match boxes function.
 
+
+
+
+//its count score and
 const countScore = function() {
   if (player1 === 'X') {
-    $('.scoreX').text(player1);
+    $('#score1').text(`${++score1}`);
 
   } else {
-    $('.scoreO').text(player1);
+
+      $('#score2').text(`${++score2}`);
   }
 }
 
 
+
+//rest the game from the start to continue playing
 const resetgame = function() {
     console.log('reset the page');
     $('.box').text("");
-
+    boardArray = ["box0", "box1", "box2", "box3", "box4", "box5", "box6", "box7", "box8"];//back to the first steop empty boardArray
   }
+
+
+
+
+ // when the sound i stop is tell you who is the winner.
+  $('#sound1')[0].addEventListener("ended", function(){
+   $('#sound1')[0].currentTime = 0;
+   console.log("ended");
+   if (score1 > score2 ){
+     $('.scoreX').text(`X is the winner`).addClass('bounceInLeft');
+
+   }else if (score1 === score2) {
+     alert('No winner, try again!')
+
+  } else {
+     $('.scoreO').text(`O is the winner`).addClass('bounceInLeft');;
+
+
+   }
+});
 
 
 //UI/ DOM
 $(document).ready(function() {
 
-  $('.box').one('click', function() {
-
+  $('.box').on('click', function() {
     let position = $(this).context.id // this refer actual element we click on in this case is whichever box. and get the id form 0-8.         // console.log(position);
-    array[position] = player1; //player1 is "X";
+    if (boardArray[position] === 'X' || boardArray[position] === 'O') {
+      return; // exit. we change one to on an add this part to continue playing.
+    }
+    boardArray[position] = player1; //player1 is "X";
+    let audio = $('#sound1')[0].play();
+
+
+
+
     $(this).context.innerHTML = player1; //player1 is"X";
     // $(this).context.innerHTML = palyer2;
 
